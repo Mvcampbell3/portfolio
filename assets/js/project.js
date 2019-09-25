6ad419632d3b78b6360cf82ce8d12f43d4b6e3a7
Mvcampbell3 <mvcampbell3@gmail.com>
2019-09-24 21:24:58 -0400

    added project animation

---
+++ assets/js/project.js
@@ -0,0 +1,75 @@
+function Project(id) {
+  this.id = id;
+  this.shown = false;
+  this.moving = false;
+
+  this.getTop = function() {
+    const divPro = document.getElementById(this.id);
+    return divPro.getBoundingClientRect().top;
+  }
+
+  this.evalTop = function() {
+    if (!this.moving) {
+      if (this.getTop() <= 450 && !this.shown) {
+        // Move project in
+        console.log("move project in")
+        this.moveRightIn()
+      } else if (this.getTop() > 450 && this.shown) {
+        // Move project out
+        console.log("move project out")
+        this.moveRightOut()
+      } else {
+        // Do nothing
+        console.log("do nothing")
+      }
+    } else {
+      console.log("animation running")
+    }
+    
+  }
+
+  this.moveRightIn = function() {
+    this.moving = true;
+    const divMove = document.getElementById(this.id);
+    const divAni = divMove.animate({
+      left: ["-100%", "0"]
+    }, {
+      duration: 700,
+      fill: "forwards"
+    })
+
+    divAni.onfinish = () => {
+      this.moving = false;
+      this.shown = true
+    }
+  }
+
+  this.moveRightOut = function() {
+    this.moving = true;
+    const divMove = document.getElementById(this.id);
+    const divAni = divMove.animate({
+      left: ["0", "-100%"]
+    }, {
+      duration: 700,
+      fill: "forwards"
+    })
+
+    divAni.onfinish = () => {
+      this.moving = false;
+      this.shown = false
+    }
+  }
+}
+
+const rover_reddit = new Project("rover-reddit");
+const yahtzee = new Project("yahtzee");
+const trivia_madness = new Project("trivia-madness");
+const skyrim = new Project("skyrim-clickgame");
+const stardew = new Project("stardew");
+const space_scrape = new Project("space-scrape");
+
+const projects = [rover_reddit, yahtzee, trivia_madness, skyrim, stardew, space_scrape]
+
+document.addEventListener("scroll", function() {
+  projects.forEach(project => project.evalTop())
+})
