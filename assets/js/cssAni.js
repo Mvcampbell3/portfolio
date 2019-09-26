bc92ee32ce6783f0f39404068b38dfeda70788be
Mvcampbell3 <mvcampbell3@gmail.com>
2019-09-26 17:25:09 -0400

    merged mobile and desktop animations

--- assets/js/cssAni.js
+++ assets/js/cssAni.js
@@ -7,9 +7,14 @@ function Project(id) {
   this.mobileSet = false;
 
   this.checkWidth = function() {
+    console.log("ran")
     if (window.innerWidth < 450) {
       // This will run first load and determine if the window's width
-      // It appears there is something going on with the animation itself
+      this.mobile = true;
+      this.mobileSet = true;
+    } else {
+      this.mobile = false;
+      this.mobileSet = true;
     }
   }
 
@@ -19,27 +24,88 @@ function Project(id) {
   }
 
   this.evalTop = function() {
-    if (!this.moving) {
-      if (this.getTop() <= 450 && !this.shown) {
-        // Move project in
-        this.moving = true;
-        const divMove = document.getElementById(this.id);
-        divMove.classList.add("animateMove");
-        setTimeout(() => {
-          this.moving = false;
-        }, 1500)
-      } else if (this.getTop() > 450 && this.shown) {
-        // Move project out
+    console.log(this.mobile, this.mobileSet)
+    if (this.mobile && this.mobileSet) {
+      // This is a smaller device
+      if (!this.moving) {
+        if (this.getTop() <= 450 && !this.shown) {
+          // Move project in
+          this.moving = true;
+          const divMove = document.getElementById(this.id);
+          divMove.classList.remove("animateMoveOut");
+          divMove.classList.add("animateMove");
+          setTimeout(() => {
+            this.moving = false;
+            this.shown = true;
+          }, 1500)
+        } else if (this.getTop() > 450 && this.shown) {
+          // Move project out
+          this.moving = true;
+          const divMove = document.getElementById(this.id);
+          divMove.classList.remove("animateMove");
+          divMove.classList.add("animateMoveOut");
+          setTimeout(() => {
+            this.moving = false;
+            this.shown = false;
+          })
+        }
+      } else {
+        // console.log("animation running")
       }
     } else {
-      // console.log("animation running")
+      // this is a desktop
+      if (!this.moving) {
+        if (this.getTop() <= 450 && !this.shown) {
+          // Move project in
+          this.moveRightIn()
+        } else if (this.getTop() > 450 && this.shown) {
+          // Move project out
+          this.moveRightOut()
+        }
+      } else {
+        // console.log("animation running")
+      }
     }
-    
+
+
   }
 
-  
+  this.moveRightIn = function() {
+    this.moving = true;
+    const divMove = document.getElementById(this.id);
+    const divAni = divMove.animate({
+      left: ["-100%", "0"]
+    }, {
+      duration: this.speed,
+      fill: "forwards"
+    })
 
-  
+    divAni.onfinish = () => {
+      this.moving = false;
+      this.shown = true
+    }
+  }
+
+  this.moveRightOut = function() {
+    this.moving = true;
+    const divMove = document.getElementById(this.id);
+    const divAni = divMove.animate({
+      left: ["0", "-100%"]
+    }, {
+      duration: this.speed,
+      fill: "forwards"
+    })
+
+    divAni.onfinish = () => {
+      this.moving = false;
+      this.shown = false
+    }
+  }
+
+
+
+
+
 }
 
 const rover_reddit = new Project("rover-reddit");
@@ -54,3 +120,5 @@ const projects = [rover_reddit, yahtzee, trivia_madness, skyrim, stardew, space_
 document.addEventListener("scroll", function() {
   projects.forEach(project => project.evalTop())
 })
+
+projects.forEach(project => project.checkWidth())
